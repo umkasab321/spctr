8ada265417e1b1d76570377f598c8b2187df2753
anago <anago@anago>
2017-07-09 17:20:28 +0900

    書式を整理

--- spctl.py
+++ spctl.py
@@ -15,30 +15,6 @@ print(x12)
 print("y12 ", end = "")
 print(y12)
 
-for var in range(0, n):
-    a2 = a1 - 4 * agree
-    x21 =  (-1 * 13 * np.tan(agree) - x12 * np.tan(a2) + y12) / (np.tan(agree) - np.tan(a2))
-    y21 =  np.tan(agree) * (x21 + 13)
-    x22 =  (13 * np.tan(agree) + x21 * np.tan(2 * agree - a2) - y21) / (np.tan(agree) + np.tan(2 * agree - a2))
-    y22 =  -1 * np.tan(agree) * (x22 - 13)
-
-    print(a2, end = "")
-    print(np.tan(a2))
-    print("x21 ", end = "")
-    print(x21)
-    print("y21 ", end = "")
-    print(y21)
-    print("x22 ", end = "")
-    print(x22)
-    print("y22 ", end = "")
-    print(y22)
-
-    a1 = a2
-    x11 = x21
-    x12 = x22
-    y11 = y21
-    y12 = y22
-
 a2 = a1 - 4 * agree
 x21 =  (-1 * 13 * np.tan(agree) - x12 * np.tan(a2) + y12) / (np.tan(agree) - np.tan(a2))
 y21 =  np.tan(agree) * (x21 + 13)
@@ -58,3 +34,39 @@ print("x22")
 print(x22)
 print("y22")
 print(y22)
+count = 0
+for var in range(0, n):
+    a2 = a1 - 4 * agree
+    x21 =  (-1 * 13 * np.tan(agree) - x12 * np.tan(a2) + y12) / (np.tan(agree) - np.tan(a2))
+    y21 =  np.tan(agree) * (x21 + 13)
+    x22 =  (13 * np.tan(agree) + x21 * np.tan(2 * agree - a2) - y21) / (np.tan(agree) + np.tan(2 * agree - a2))
+    y22 =  -1 * np.tan(agree) * (x22 - 13)
+
+    print(a2, end = "")
+    print(np.tan(a2))
+    print("x", end = "")
+    print(count + 2, end = "")
+    print("1 ", end = "")
+    print(x21)
+    print("y", end = "")
+    print(count + 2, end = "")
+    print("1 ", end = "")
+    print(y21)
+    print("x", end = "")
+    print(count + 2, end = "")
+    print("2 ", end = "")
+    print(x22)
+    print("y", end = "")
+    print(count + 2, end = "")
+    print("2 ", end = "")
+    print(y22)
+    print("")
+
+    a1 = a2
+    x11 = x21
+    x12 = x22
+    y11 = y21
+    y12 = y22
+
+    count += 1
+
